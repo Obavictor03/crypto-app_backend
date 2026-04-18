1e4551f4ed5ad6bf938caece0124951631f74d80
Obavictor03 <obarovictor03@gmail.com>
2026-04-19 00:48:02 +0100

    custom admin wallet added

--- wallet/admin.py
+++ wallet/admin.py
@@ -3,6 +3,7 @@ from .models import Wallet
 from django import forms
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.admin.helpers import ACTION_CHECKBOX_NAME
 
 class AddBTCForm(forms.Form):
     _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
@@ -10,27 +11,34 @@ class AddBTCForm(forms.Form):
 
 @admin.action(description="Add BTC (custom amount)")
 def add_btc(modeladmin, request, queryset):
+    from django.contrib.admin.helpers import ACTION_CHECKBOX_NAME
+
     if 'apply' in request.POST:
         form = AddBTCForm(request.POST)
 
         if form.is_valid():
             amount = form.cleaned_data['amount']
 
-            for wallet in queryset:
+            selected = request.POST.getlist(ACTION_CHECKBOX_NAME)
+
+            wallets = Wallet.objects.filter(pk__in=selected)
+
+            for wallet in wallets:
                 wallet.add_btc(amount)
 
             messages.success(request, f"{amount} BTC added successfully")
             return redirect(request.get_full_path())
 
-    else:
-        form = AddBTCForm(initial={
-            '_selected_action': request.POST.getlist('_selected_action')
-        })
+        else:
+            form = AddBTCForm(initial={
+                '_selected_action': request.POST.getlist(ACTION_CHECKBOX_NAME)
+            })
 
-    return render(request, "admin/add_btc.html", {
-        "wallets": queryset,
-        "form": form,
-    })
+        return render(request, "admin/add_btc.html", {
+            "wallets": queryset,
+            "form": form,
+            "action_checkbox_name": ACTION_CHECKBOX_NAME,
+        })
 
 @admin.register(Wallet)
 class WalletAdmin(admin.ModelAdmin):
