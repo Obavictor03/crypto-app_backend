da34550a4c3d243a6128c1a659166279c364995c
Obavictor03 <obarovictor03@gmail.com>
2026-04-18 21:01:09 +0100

    fix admin bug

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from .models import Wallet
-# Register your models here.
-
 
 @admin.register(Wallet)
 class WalletAdmin(admin.ModelAdmin):
     list_display = ("user", "btc_balance", "get_balance")
+    readonly_fields = ("get_balance",)
 
     def get_balance(self, obj):
         return obj.balance
 
     get_balance.short_description = "USD Balance"
 
-    # ❌ Prevent editing USD
-    readonly_fields = ("get_balance",)
+    # ❌ Disable adding new wallets manually
+    def has_add_permission(self, request):
+        return False
